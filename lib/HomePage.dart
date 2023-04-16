0277e916a051698b8ba3dfa5d007aa0813e149d7
rmit2 <rmit25@mail.ru>
2023-04-16 09:40:51 +0300

    new starting preparation, constants, and prepare environment

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,41 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note/constant.dart';
 
-import 'notes.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List notesList = [
-    ["First note", false],
-    ["Second note", false],
-    ["Third note", false],
-    ["Fourth note", true],
-    ["Fifth note", true],
-    ["Sixth note", true],
-    ["Seventh note", false],
-  ];
-
-  void checkBoxChanged(bool value,int index){
-
-  }
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: tdBGColor,
       appBar: AppBar(
+        backgroundColor: tdBGColor,
         elevation: 0,
       ),
-      body: ListView.builder(
-        itemCount: notesList.length,
-        itemBuilder: (context, index) {
-          return Notes(
-              noteName: notesList[index][0], noteCompleted: notesList[index][1],
-              onChanged: (value)=>checkBoxChanged);
-        },
+      body: Container(
+        child: Text("123"),
       ),
     );
   }
